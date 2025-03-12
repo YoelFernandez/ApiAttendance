88e1e7578abc3761327938ef667f220c8125ca0e
Yoel Fernandez <afernandezs22_1@unc.edu.pe>
2025-03-12 00:24:24 -0500

    añadiendo la conexion con variables de entorno

--- src/main/java/com/yoel/fernandez/ApiAttendance/ApiAttendanceApplication.java
+++ src/main/java/com/yoel/fernandez/ApiAttendance/ApiAttendanceApplication.java
@@ -3,10 +3,18 @@ package com.yoel.fernandez.ApiAttendance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiAttendanceApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+	
+	
 		SpringApplication.run(ApiAttendanceApplication.class, args);
 	}
 
