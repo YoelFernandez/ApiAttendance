e7b6f6b18a7aebc0a754d8757b8080c7d52b18ad
Yoel Fernandez <afernandezs22_1@unc.edu.pe>
2025-04-10 23:08:19 -0500

    añadiendo el cors para aceptar peticiones desde angular

--- src/main/java/com/yoel/fernandez/ApiAttendance/Config/SecurityConfig.java
+++ src/main/java/com/yoel/fernandez/ApiAttendance/Config/SecurityConfig.java
@@ -15,11 +15,13 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer{
 
     private final UserDetailsService userDetailsService;
 
@@ -41,7 +43,11 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/employed/probandoc").permitAll()
+                .requestMatchers("/obra/listarDTO").permitAll()
+                .requestMatchers("/client/nuevoDTO").permitAll()
+                .requestMatchers("/client/listarDTO").permitAll()
+                .requestMatchers("/client/actualizar/**").permitAll()  // Permite actualizaciones de clientes
+                .requestMatchers("/client/**").permitAll()  // Permite obtener cliente por ID
                 .requestMatchers("/api/auth/login").permitAll()
                 .requestMatchers("/private/**").hasAuthority("ADMIN")
                 .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
@@ -56,5 +62,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    // Habilitar CORS globalmente
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200/", "https://tu-dominio.com") // Los orígenes permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite credenciales como cookies o encabezados Authorization
     }
 }
